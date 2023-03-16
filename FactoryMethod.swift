6106d5f639515062b93edeb5282a21b34552fc5c
CHia <dozeuto@gmail.com>
2023-03-16 13:35:58 +0800

    factory method

--- FactoryMethod.swift
+++ FactoryMethod.swift
@@ -1,23 +1,86 @@
-import Foundation
 
-protocol Projector {
-    var currentPage: Int { get }
-
-    func present(info: String)
-
-    func sync(with projector: Projector)
-
-    func update(with page: Int)
+/// The Product protocol declares the operations that all concrete products must
+/// implement.
+protocol Product {
+    func operation() -> String
 }
 
-extension Projector {
-    func sync(with projector: Projector) {
-        projector.update(with: currentPage)
+/// Concrete Products provide various implementations of the Product protocol.
+class ConcreteProduct1: Product {
+    func operation() -> String {
+        return "{Result of the ConcreteProduct1}"
     }
 }
 
-protocol ProjectorFactory {
-    func createProjector()
-
-    func syncedProjector()
+class ConcreteProduct2: Product {
+    func operation() -> String {
+        return "{Result of the ConcreteProduct2}"
+    }
 }
+
+/// The Creator protocol declares the factory method that's supposed to return a
+/// new object of a Product class. The Creator's subclasses usually provide the
+/// implementation of this method.
+protocol Creator {
+
+    /// Note that the Creator may also provide some default implementation of
+    /// the factory method.
+    func factoryMethod() -> Product
+    
+    /// Also note that, despite its name, the Creator's primary responsibility
+    /// is not creating products. Usually, it contains some core business logic
+    /// that relies on Product objects, returned by the factory method.
+    /// Subclasses can indirectly change that business logic by overriding the
+    /// factory method and returning a different type of product from it.
+    func someOperation() -> String
+}
+
+/// This extension implements the default behavior of the Creator. This behavior
+/// can be overridden in subclasses.
+extension Creator {
+    func someOperation() -> String {
+        // Call the factory method to create a Product object.
+        let product = factoryMethod()
+
+        // Now, use the product.
+        return "Creator: The same creator's code has just worked with " + product.operation()
+    }
+}
+
+/// Concrete Creators override the factory method in order to change the
+/// resulting product's type.
+class ConcreteCreator1: Creator {
+
+    /// Note that the signature of the method still uses the abstract product
+    /// type, even though the concrete product is actually returned from the
+    /// method. This way the Creator can stay independent of concrete product
+    /// classes.
+    func factoryMethod() -> Product {
+        return ConcreteProduct1()
+    }
+
+    func someOperation() -> String {
+        // factoryMethod()
+        return "\(self): this is mine own operation, not others"
+    }
+}
+
+class ConcreteCreator2: Creator {
+    func factoryMethod() -> Product {
+        return ConcreteProduct2()
+    }
+}
+
+/// The client code works with an instance of a concrete creator, albeit through
+/// its base protocol. As long as the client keeps working with the creator via
+/// the base protocol, you can pass it any creator's subclass.
+class Client {
+    static func someClientCode(creator: Creator) {
+        print("Client: I'm nor aware of the creator's class, but it will works.\n" + creator.someOperation())
+    }
+}
+
+print("App: Launched with the ConcreteCreator1.")
+Client.someClientCode(creator: ConcreteCreator1())
+print("App: Launched with the ConcreteCreator2.")
+Client.someClientCode(creator: ConcreteCreator2())
